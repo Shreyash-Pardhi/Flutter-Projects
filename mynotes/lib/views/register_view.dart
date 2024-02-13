84b6ce09bba154ca996669e7c291f86cdcea42d8
Shreyash Pardhi <shreyashpardhi1@gmail.com>
2024-02-14 01:33:37 +0530

    cleaning up the routes

--- mynotes/lib/views/register_view.dart
+++ mynotes/lib/views/register_view.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
+import 'package:mynotes/constants/routes.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -71,7 +73,7 @@ class _RegisterViewState extends State<RegisterView> {
           TextButton(
               onPressed: () {
                 Navigator.of(context).pushNamedAndRemoveUntil(
-                  '/login/',
+                  loginRoute,
                   (route) => false,
                 );
               },
